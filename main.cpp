63b1bc7b55bb35004f3d597c145626ccfa2ef42b
sledge79 <tonywalsh1511@gmail.com>
2023-07-28 13:45:17 +0000

    First Commit
    Signed-off-by: sledge79 <tonywalsh1511@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
+// Why this occured: CO_E_WRONG_SERVER_IDENTITY
+
+
+
 #include "mbed.h"
 
 // main() runs in its own thread in the OS
+InterruptIn bttn(PC_13);
+    void bttn_interrupt(){
+    printf("bttn pressed /r/n");
+    }
 int main()
-{
+{   
+    bttn.fall(&bttn_interrupt);
     while (true) {
 
     }
