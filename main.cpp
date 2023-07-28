b10e37c799d22ab469008b1c7699925a1c48c214
sledge79 <tonywalsh1511@gmail.com>
2023-07-28 14:18:05 +0000

    Second Commit
    Signed-off-by: sledge79 <tonywalsh1511@gmail.com>

--- main.cpp
+++ main.cpp
@@ -4,16 +4,28 @@
 
 #include "mbed.h"
 
+    int flag = 0;
+
 // main() runs in its own thread in the OS
 InterruptIn bttn(PC_13);
     void bttn_interrupt(){
     printf("bttn pressed /r/n");
+    flag = 1;
     }
 int main()
 {   
     bttn.fall(&bttn_interrupt);
-    while (true) {
+   while (1) {
+        if (flag) {
+            printf("Flag is set!\n");
+            flag = 0; // Reset the flag
+        }
+        // Other code and logic inside the while loop
+    }
+
+    return 0;
+
 
     }
-}
+
 
