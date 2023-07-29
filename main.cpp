ac773f2722f9dc36b623c61a0d7ce41f5298f375
sledge79 <tonywalsh1511@gmail.com>
2023-07-29 14:23:51 +0000

    Final Build
    Signed-off-by: sledge79 <tonywalsh1511@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,31 +1,27 @@
 // Why this occured: CO_E_WRONG_SERVER_IDENTITY
 
-
-
 #include "mbed.h"
 
-    int flag = 0;
+InterruptIn button(PC_13); // Assuming button is connected to pin PA_5
+volatile bool flag = false;
 
-// main() runs in its own thread in the OS
-InterruptIn bttn(PC_13);
-    void bttn_interrupt(){
-    printf("bttn pressed /r/n");
-    flag = 1;
-    }
-int main()
-{   
-    bttn.fall(&bttn_interrupt);
-   while (1) {
+void button_isr() {
+    flag = true;
+}
+
+int main() {
+    button.rise(&button_isr); // Attach the interrupt function to the rising edge of the button
+    while (1) {
         if (flag) {
-            printf("Flag is set!\n");
-            flag = 0; 
+            printf("Button pressed!\r\n");
+            flag = false; // Reset the flag
         }
-        
+        // Other code and logic inside the while loop
     }
 
     return 0;
+}
 
 
-    }
 
 
